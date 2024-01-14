759da0f5e9917c43f8dde0efe3675688db8bf653
Alexander <a.kireev.qa@gmail.com>
2024-01-14 20:01:25 +0300

    refactor: доработать UsersQueueExtension под несколько параметров и проброс в before / test методы, добавить юнит тесты

--- niffler-e-2-e-tests-student/src/test/java/guru/qa/niffler/jupiter/UsersQueueExtension.java
+++ niffler-e-2-e-tests-student/src/test/java/guru/qa/niffler/jupiter/UsersQueueExtension.java
@@ -1,79 +1,83 @@
 package guru.qa.niffler.jupiter;
 
+import static guru.qa.niffler.jupiter.User.UserType.COMMON;
+import static guru.qa.niffler.jupiter.User.UserType.WITH_FRIENDS;
+
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.TestData;
 import guru.qa.niffler.model.UserJson;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 
-import java.lang.reflect.Parameter;
-import java.util.Map;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import static guru.qa.niffler.jupiter.User.UserType.COMMON;
-import static guru.qa.niffler.jupiter.User.UserType.WITH_FRIENDS;
-
-public class UsersQueueExtension implements BeforeEachCallback, AfterTestExecutionCallback, ParameterResolver {
+public class UsersQueueExtension implements AfterTestExecutionCallback, ParameterResolver {
 
   public static final ExtensionContext.Namespace NAMESPACE
       = ExtensionContext.Namespace.create(UsersQueueExtension.class);
-
-  private static Map<User.UserType, Queue<UserJson>> users = new ConcurrentHashMap<>();
+  private static final Map<User.UserType, Queue<UserJson>> users = new ConcurrentHashMap<>();
 
   static {
     Queue<UserJson> friendsQueue = new ConcurrentLinkedQueue<>();
     Queue<UserJson> commonQueue = new ConcurrentLinkedQueue<>();
     friendsQueue.add(user("dima", "12345", WITH_FRIENDS));
     friendsQueue.add(user("duck", "12345", WITH_FRIENDS));
-    commonQueue.add(user("bee", "12345", COMMON));
+    commonQueue.add(user("bee", "123", COMMON));
     commonQueue.add(user("barsik", "12345", COMMON));
     users.put(WITH_FRIENDS, friendsQueue);
     users.put(COMMON, commonQueue);
   }
 
+  @SuppressWarnings("unchecked")
   @Override
-  public void beforeEach(ExtensionContext context) throws Exception {
-    Parameter[] parameters = context.getRequiredTestMethod().getParameters();
+  public void afterTestExecution(ExtensionContext context) {
+    var usersFromTest = context.getStore(NAMESPACE).get(context.getUniqueId(), List.class);
+    usersFromTest.forEach(s -> {
+      var userFromTest = (UserJson) s;
+      users.get(userFromTest.testData().userType()).add(userFromTest);
+    });
+  }
 
-    for (Parameter parameter : parameters) {
-      User annotation = parameter.getAnnotation(User.class);
-      if (annotation != null && parameter.getType().isAssignableFrom(UserJson.class)) {
-        UserJson testCandidate = null;
-        Queue<UserJson> queue = users.get(annotation.value());
-        while (testCandidate == null) {
-          testCandidate = queue.poll();
-        }
-        context.getStore(NAMESPACE).put(context.getUniqueId(), testCandidate);
-        break;
-      }
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+      throws ParameterResolutionException {
+    return parameterContext.getParameter().getType().isAssignableFrom(UserJson.class)
+        && parameterContext.getParameter().isAnnotationPresent(User.class);
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+      throws ParameterResolutionException {
+    var user = getUserFromPool(parameterContext.getParameter());
+    addUserToStore(extensionContext, user);
+    return extensionContext.getStore(NAMESPACE).get(extensionContext.getUniqueId(), List.class)
+        .get(parameterContext.getIndex());
+  }
+
+  @SuppressWarnings("unchecked")
+  private void addUserToStore(ExtensionContext context, UserJson user) {
+    if (context.getStore(NAMESPACE).get(context.getUniqueId()) == null) {
+      context.getStore(NAMESPACE).put(context.getUniqueId(), new ArrayList<>(List.of(user)));
+    } else {
+      context.getStore(NAMESPACE).get(context.getUniqueId(), List.class).add(user);
     }
   }
 
-  @Override
-  public void afterTestExecution(ExtensionContext context) throws Exception {
-    UserJson userFromTest = context.getStore(NAMESPACE)
-        .get(context.getUniqueId(), UserJson.class);
-    users.get(userFromTest.testData().userType()).add(userFromTest);
-  }
-
-  @Override
-  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-    return parameterContext.getParameter()
-        .getType()
-        .isAssignableFrom(UserJson.class) &&
-        parameterContext.getParameter().isAnnotationPresent(User.class);
-  }
-
-  @Override
-  public UserJson resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-    return extensionContext.getStore(NAMESPACE)
-        .get(extensionContext.getUniqueId(), UserJson.class);
+  private UserJson getUserFromPool(Parameter parameter) {
+    UserJson testCandidate = null;
+    var queue = users.get(parameter.getAnnotation(User.class).value());
+    while (testCandidate == null) {
+      testCandidate = queue.poll();
+    }
+    return testCandidate;
   }
 
   private static UserJson user(String username, String password, User.UserType userType) {
---
+++ niffler-e-2-e-tests-student/src/test/java/guru/qa/niffler/test/unit/UsersResolverOnlyBeforeTest.java
@@ -0,0 +1,50 @@
+package guru.qa.niffler.test.unit;
+
+import static guru.qa.niffler.jupiter.User.UserType.COMMON;
+import static guru.qa.niffler.jupiter.User.UserType.WITH_FRIENDS;
+
+import guru.qa.niffler.jupiter.User;
+import guru.qa.niffler.jupiter.UsersQueueExtension;
+import guru.qa.niffler.model.UserJson;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Slf4j
+@ExtendWith(UsersQueueExtension.class)
+class UsersResolverOnlyBeforeTest {
+
+  private UserJson userWithFriends;
+  private UserJson commonUser;
+
+  @BeforeEach
+  void doLogin(@User(WITH_FRIENDS) UserJson userWithFriends, @User(COMMON) UserJson commonUser) {
+    log.info("UserData: {}", userWithFriends);
+    log.info("UserData: {}", commonUser);
+
+    Assertions.assertAll(
+        () -> Assertions.assertNotNull(userWithFriends),
+        () -> Assertions.assertEquals(WITH_FRIENDS, userWithFriends.testData().userType()),
+        () -> Assertions.assertNotNull(commonUser),
+        () -> Assertions.assertEquals(COMMON, commonUser.testData().userType())
+    );
+
+    this.userWithFriends = userWithFriends;
+    this.commonUser = commonUser;
+  }
+
+  @Test
+  void friendsTableShouldNotBeEmpty() {
+    log.info("UserData: {}", userWithFriends);
+    log.info("UserData: {}", commonUser);
+
+    Assertions.assertAll(
+        () -> Assertions.assertNotNull(userWithFriends),
+        () -> Assertions.assertEquals(WITH_FRIENDS, userWithFriends.testData().userType()),
+        () -> Assertions.assertNotNull(commonUser),
+        () -> Assertions.assertEquals(COMMON, commonUser.testData().userType())
+    );
+  }
+}
